f7bdcc8ac00e0adc4dc0257dd22bb19c6e9a086b
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2026-02-25 17:59:22 -0800

    Prepare package for npm release

--- src/export/workout-exporter.ts
+++
@@ -1,7 +0,0 @@
-import { WorkoutSegmentItem } from '../workouts/workout-types';
-
-interface WorkoutExporter<T> {
-  export(workoutSegments: WorkoutSegmentItem[]): Promise<T>;
-}
-
-export default WorkoutExporter;
